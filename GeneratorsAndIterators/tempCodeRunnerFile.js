af92d772bd090c8016b25922dbf0f99b59dda359
StudentPP1 <mishamakytonin@gmail.com>
2025-02-25 00:32:41 +0200

    add .gitignore to lab 1

--- GeneratorsAndIterators/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-import { incrementGenerator } from "./generators/incrementGenerator.js";
-const generator = incrementGenerator(0);
-console.log(generator.next().value)
-console.log(generator.next().value)
-console.log(generator.next().value)
-console.log(generator.next().value)
